70b765ad54126e7110d5fbd3c7e6cbe08665e001
zx <112702785+zx-yk@users.noreply.github.com>
2024-02-21 11:32:38 +0800

    Update get_num.py

--- get_num.py
+++ get_num.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://cps.hisense.com/customerAth/activity-manage/activityUser/getActivityInfo?code=74f51fd29cea445e9b95eb0dd14fba40"
+url = "https://cps.hisense.com/customerAth/activity-manage/activityUser/getActivityInfo?code=a55ca53d96bd43be81c0df7ced7ef2b0"
 
 headers = {
     "Host": "cps.hisense.com",
@@ -13,7 +13,7 @@ headers = {
     "Sec-Fetch-Site": "same-origin",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Dest": "empty",
-    "Referer": "https://cps.hisense.com/static/game_sign.shtml?code=74f51fd29cea445e9b95eb0dd14fba40",
+    "Referer": "https://cps.hisense.com/static/game_sign.shtml?code=a55ca53d96bd43be81c0df7ced7ef2b0",
     "Accept-Encoding": "gzip, deflate",
     "Cookie": "BIGipServerPOOL_DMZOCP_9080=!aAXEKnUbs0gx9QkR4GLrgeLVCXJLUz09oadC9gaZuDlLcwI05eRQ/A3usKMMQgzFjh7hd8hMnM3XGak=; TOKEN_ACTIVITY=MWE2THNPQUVwOUlHS1g0bjN1aW5naHh0a2JabVhCUkRXMDN4SWhhQkxoeExaMVNFaHEwZURHZzY5Z2dQQWhNa0MtN3VFZDl6NjhaZmp3dmx6dlQwZ3hMc1A1bFd5Q2Jkd3VsbmRjR2xwOWlMOXdwX2YtZkxxaTl2NG5uMUI2alprWDc2TFZEMHhtTkhRRzVGNTJ0YkFyNUcyN3FDYmhQZzBPUWFxZkkyck9UQTFPNXM5Z3BteXdRcGtuMndrVWwxN21SMk83czlEVQ; SCORE_CENTER_IAM_AUTH_ID=cc71eb82-a5f7-4476-84e7-99fc579eb655; SCORE_SCENTER=YXBpOmdhdGV3YXk6bG9naW46dXNlcjoxMDU2MzIxNTM=; BIGipServerPOOL_OCP_JUCLOUD_80=!wjePA/qhRmz3IAslLGyqlytKq47lSI/2iW462cTKa6ETdCTkbZjOtHspF/nlSINZWBEOsUFFJhN+5F0=",
     "Accept-Language": "zh-CN,zh;q=0.9"
